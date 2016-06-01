3c29c77bc0b09f2c4348df655213abc9c8752d58
Ashli <ashli@gaiagps.com>
2016-06-01 13:48:22 -0500

    hangman practice

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+BASE_BASE_TEMPLATE = "base.html"
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.conf.urls import include, url
 from django.contrib import admin
+from polls import views
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name='home')
+
 ]
