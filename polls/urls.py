4c5dd6b34290d86c75a2004644e0cfcb08a2f62a
Ashli <ashli@gaiagps.com>
2016-07-11 21:35:57 -0500

    add new url

--- polls/urls.py
+++ polls/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'list/', views.list, name='list'),
 ]
