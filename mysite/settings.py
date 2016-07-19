857d7bd5058fc268e9640f9a5c3716ca7a50cb48
ashlibaldwin <ashli@gaiagps.com>
2016-07-18 21:21:02 -0400

    set debug to false in master branch for deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wxurz@nh=aznf@ysp$%&jq!6fik+(i*7*-#yt#$is*&8tgqtr('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
