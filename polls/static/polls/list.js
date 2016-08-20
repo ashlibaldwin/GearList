a6ee7c4ca2b3f043be187de043d7c1ea8e3ac065
Ashli <ashli@gaiagps.com>
2016-08-20 14:19:44 -0500

    tweaks to lists page

--- polls/static/polls/list.js
+++ polls/static/polls/list.js
@@ -5,13 +5,13 @@ function myFunction() {
     
 }
 
-function ran_col() { //function name
+//function ran_col() { //function name
 
-    var color = '#'; // hexadecimal starting symbol
-    var letters = ['000000','FF0000','00FF00','0000FF','FFFF00','00FFFF','FF00FF','C0C0C0']; //Set your colors here
-    color += letters[Math.floor(Math.random() * letters.length)];
+   // var color = '#'; // hexadecimal starting symbol
+  //  var letters = ['000000','FF0000','00FF00','0000FF','FFFF00','00FFFF','FF00FF','C0C0C0']; //Set your colors here
+ //   color += letters[Math.floor(Math.random() * letters.length)];
 
     
-    	document.getElementById('lists').style.background = color; // Setting the random color on your div element.
+   // 	document.getElementById('list-color').style.background = color; // Setting the random color on your div element.
 
-}
+//}
