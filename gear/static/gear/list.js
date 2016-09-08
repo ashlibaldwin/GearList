77608e0fc5b8ade8d991ed1db92374ea706e4dbd
ashlibaldwin <ashli@gaiagps.com>
2016-09-08 18:44:38 -0500

    make checkboxes stay checked

--- gear/static/gear/list.js
+++ gear/static/gear/list.js
@@ -15,3 +15,21 @@ function myFunction() {
    // 	document.getElementById('list-color').style.background = color; // Setting the random color on your div element.
 
 //}
+
+
+//keep boxed checked and store state locally
+var checkboxValues = JSON.parse(localStorage.getItem('checkboxValues')) || {},
+    $checkboxes = $("#checkbox-container :checkbox");
+
+$checkboxes.on("click", function(){
+    checkboxValues[this.id] = this.checked;
+  
+  localStorage.setItem("checkboxValues", JSON.stringify(checkboxValues));
+});
+
+// On page load
+$.each(checkboxValues, function(key, value) {
+  $("#" + key).prop('checked', value);
+});
+
+
