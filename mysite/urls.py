bf2a878f7f1460657a17a450f4861db4d5dfd72c
Ashli <ashli@gaiagps.com>
2016-09-10 11:19:58 -0500

    add forgot password

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,8 +19,8 @@ from django.contrib import admin
 from gear import views
 
 urlpatterns = [
-    url(r'^gear/', include('gear.urls')),
+    url(r'^gear/', include('gear.urls', namespace="gear")),
     url(r'^admin/', admin.site.urls),
-    
     url(r'^$', views.home, name='home'),
+    url(r'^passwordreset/', include('password_reset.urls')),
 ]
