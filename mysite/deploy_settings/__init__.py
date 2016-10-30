6849090c5a6fa4289ffc1182bea70cf18bde1642
Ashli <ashli@gaiagps.com>
2016-10-30 12:45:10 -0500

    settings for mysql

--- mysite/deploy_settings/__init__.py
+++ mysite/deploy_settings/__init__.py
@@ -1,3 +1,4 @@
+import dj_database_url
 from ..settings import *
 
 DEBUG = False
@@ -10,4 +11,5 @@ ALLOWED_HOSTS = [
 ]
 
 SECRET_KEY = get_env_variable("SECRET_KEY")
-
+db_from_env = dj_database_url.config()
+DATABASES["default"].update(db_from_env)
