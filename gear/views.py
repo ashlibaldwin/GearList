a921d9ef3fee62f6eab6e7acde38114c9a775f9c
ashlibaldwin <ashli@gaiagps.com>
2016-12-23 17:11:35 -0500

    keep the login error message on the page

--- gear/views.py
+++ gear/views.py
@@ -182,8 +182,8 @@ def user_login(request):
                 return HttpResponse("Your account is disabled.")
         else:
             # Bad login details were provided. So we can't log the user in.
-            print ("Invalid login details: {0}, {1}".format(username, password))
-            return HttpResponse("Invalid login details supplied.")
+            return render(request, 'gear/login.html',{
+            'login_message' : 'Username and Password do not match',})
 
     # The request is not a HTTP POST, so display the login form.
     # This scenario would most likely be a HTTP GET.
