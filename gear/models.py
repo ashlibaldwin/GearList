d4eb394e84cd77b4fd39475e94ea3975433a76c2
ashlibaldwin <ashli@gaiagps.com>
2019-04-14 19:16:15 -0700

    WOOT

--- gear/models.py
+++ gear/models.py
@@ -9,26 +9,27 @@ from django.contrib.auth.models import User
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, related_name='user', on_delete=models.CASCADE)
-    website = models.URLField(blank=True)
-    picture = models.ImageField(upload_to='profile_images', blank=True)
+  user = models.OneToOneField(User, related_name='user', on_delete=models.CASCADE)
+  website = models.URLField(blank=True)
+  picture = models.ImageField(upload_to='profile_images', blank=True)
 
-    def __unicode__(self):
-        return self.user.username
+  def __unicode__(self):
+    return self.user.username
 
 
 class List(models.Model): 
-    title = models.CharField("", max_length=250, unique=False, editable=True) 
-    user = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
+  title = models.CharField("", max_length=250, unique=False, editable=True) 
+  user = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
     
-    def __str__(self): 
-        return self.title
+  def __str__(self): 
+      return self.title
 
 
 class Item(models.Model): 
   title = models.CharField("", max_length=250, unique=False) 
   created_date = models.DateTimeField(default=datetime.datetime.now) 
   todo_list = models.ForeignKey(List, on_delete=models.CASCADE)
+  checked = models.BooleanField(default=False)
 
   def __str__(self): 
     return self.title 
